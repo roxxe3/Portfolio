e77c4dd86914a8af76f770bff1dc98462170b611
roxxe34 <hamzafarissi7@gmail.com>
2024-10-25 12:32:35 +0100

    update career page

--- src/components/CareerSection.jsx
+++ src/components/CareerSection.jsx
@@ -11,102 +11,88 @@ const updatedAt = new Date('2022-10-28T08:00:00.000Z');
 const CareerSection = () => (
   <Section updatedAt={updatedAt}>
     <Content sx={{ gap: 8 }}>
-      <Article
-        icon={<Emoji symbol="‍‍👾" label="alien monster" />}
-        title="Discord"
-        subtitle="September 2022 to Present"
-      >
-        <Text>
-          I am a software engineer on the Premium Products team at{' '}
-          <Link
-            href="https://discord.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Discord
-          </Link>
-          , building Nitro subscriber perks.
-        </Text>
-      </Article>
-
-      <Article
-        icon={<Emoji symbol="‍👩‍🍳" label="woman cook" />}
-        title="Shef"
-        subtitle="April 2021 to July 2022"
-      >
-        <Text>
-          I was a software engineer at{' '}
-          <Link
-            href="https://shef.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Shef (YC W19)
-          </Link>
-          , where I developed the consumer web experience, chef onboarding and supply
-          platform, and internal administrator tooling.
-        </Text>
-      </Article>
-
-      <Article
-        icon={<Emoji symbol="🐨" label="koala" />}
-        title="Koala"
-        subtitle="September 2019 to March 2021"
-      >
-        <Text>
-          I was the second and lead engineer at{' '}
-          <Link
-            href="https://teachwithkoala.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Koala (YC W22)
-          </Link>
-          , where I worked on a 3D collaborative classroom application for
-          independent tutors. I launched parent, tutor, and administrator web
-          platforms for our Oculus VR Spanish and reading tutoring service, and
-          worked together with the CTO on the primary Unity and Electron
-          products supported on Mac, Windows, and WebGL.
-        </Text>
-      </Article>
-
-      <Article
-        icon={<Emoji symbol="🌁" label="bridge under fog" />}
+    <Article
+        icon={<Emoji symbol="💻" label="Coding" />}
         title="Holberton School"
-        subtitle="September 2018 to June 2019"
+        subtitle="October 2023 to October 2024"
       >
         <Text>
           I studied software engineering at{' '}
           <Link
-            href="https://holbertonschool.com"
+            href="https://intranet.alxswe.com/certificates/FzYNM5Jpse"
             target="_blank"
             rel="noopener noreferrer"
           >
             Holberton School
           </Link>
-          , San Francisco, where I completed the nine-month computer science,
-          programming and full-stack web development curriculum.
+          , where I gained a comprehensive understanding of programming languages and development tools ⚙️. 
+          My coursework included practical applications of C, Python, JavaScript, and SQL, alongside DevOps practices and web development 🌐. 
+          I engaged in collaborative projects 🚀, enhancing my problem-solving skills and ability to design efficient software solutions 💡.
         </Text>
       </Article>
 
       <Article
-        icon={<Emoji symbol="🐄" label="cow" />}
-        title="UC Davis"
-        subtitle="September 2015 to June 2018"
+        icon={<Emoji symbol="☁️🚀" label="Cloud ++" />}
+        title="AWS Solutions Architect - Associate Certification"
+        subtitle="November 2023 to February 2024"
       >
         <Text>
-          I attended the University of California, Davis as an undergraduate,
-          where I received a B.A. in{' '}
+        In preparation for the{' '}
           <Link
-            href="https://americanstudies.ucdavis.edu/"
+            href="https://www.credly.com/badges/efbd5518-a52f-4da0-be15-6c8208792501/linked_in_profile"
             target="_blank"
             rel="noopener noreferrer"
           >
-            American Studies
+          AWS Solutions Architect - Associate certification,
           </Link>
-          , an interdisciplinary field in the humanities that studies the
-          construction of American culture. I additionally completed a double
-          minor in Statistics and Economics.
+          I deepened my understanding of architectural best practices for designing and deploying scalable,
+           highly available, and fault-tolerant systems on AWS. My studies covered key AWS services,
+            including compute, storage, networking, and security, along with strategies for cost optimization and architectural patterns,
+             equipping me to effectively design cloud solutions that meet business needs.
+        </Text>
+      </Article>
+
+      <Article
+        icon={<Emoji symbol="☁️" label="Cloud" />}
+        title="AWS Cloud Practitioner Certification"
+        subtitle="September 2023 to October 2023"
+      >
+        <Text>
+        During my studies for the {' '}
+          <Link
+            href="https://www.credly.com/badges/8a5e000c-35e4-43a3-b482-4ddaae4ab31b/linked_in_profile"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          AWS Cloud Practitioner certification,
+          </Link>
+           I gained foundational knowledge of Amazon Web Services, 
+           focusing on key concepts such as cloud computing, AWS services, 
+           architecture best practices, and basic security measures. 
+           This program equipped me with the skills to understand cloud deployment, 
+           management, and cost optimization, preparing me for further exploration in cloud technologies.
+        </Text>
+      </Article>
+
+      <Article
+        icon={<Emoji symbol="🏫" label="university" />}
+        title="FSJES Ain Chock "
+        subtitle="September 2019 to June 2023"
+      >
+        <Text>
+        I attended the {' '}
+        <Link
+            href="https://www.fdc.ma/FDC/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          University Hassan II
+          </Link>
+        as an undergraduate,
+        where I received a Bachelor's Degree in Economics and Management,
+        focusing on the principles of economics and management.
+        This program provided an interdisciplinary foundation in economic theories,
+        business management, and quantitative methods.
         </Text>
       </Article>
     </Content>
